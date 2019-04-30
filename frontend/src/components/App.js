93a3c4bcfafa6d7f7186385c49640c739e48dca9
Mateusz Pernal <exomat@wp.pl>
2019-04-30 22:14:00 +0200

    fix frontend problem with index.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Header from "./layout/Header";
 
 class App extends Component {
     render() {
-        return <h1>test</h1>
+        return (
+            <Header/>
+        )
+
+
     }
 }
 
