d47f5d2a75555e92b2a087b2720a334cafb4dbc0
Mateusz Pernal <exomat@wp.pl>
2019-05-01 15:27:51 +0200

    add message box :3

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,10 +5,13 @@ import ReactDOM from 'react-dom';
 import Header from "./layout/Header";
 import Experiments from "./experiments/Experiments";
 
+import {Provider as AlertProvider} from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 import {Provider} from "react-redux";
 import store from "../store";
 import FormExperiment from "./experiments/FormExperiment";
+import Alerts from "./layout/Alerts";
 
 const alertOptions = {
     timeout: 3000,
@@ -19,13 +22,16 @@ class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header/>
-                    <div className="container">
-                        <Experiments/>
-                        <FormExperiment/>
-                    </div>
-                </Fragment>
+                <AlertProvider template={AlertTemplate} {...alertOptions}>
+                    <Fragment>
+                        <Header/>
+                        <Alerts/>
+                        <div className="container">
+                            <Experiments/>
+                            <FormExperiment/>
+                        </div>
+                    </Fragment>
+                </AlertProvider>
             </Provider>
         )
 
