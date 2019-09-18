e868d0fd7a61f5307d5ea629247792e35c8ba031
Mateusz Pernal <exomat@wp.pl>
2019-09-18 23:14:39 +0200

    Add modal show popup dialog.

--- frontend/src/components/experiments/Experiments.js
+++ frontend/src/components/experiments/Experiments.js
@@ -3,7 +3,8 @@ import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {deleteExperiment, getExperiments, getExperimentById} from "../../actions/experiments";
 import {Link} from "react-router-dom";
-
+import {Modal} from "react-bootstrap"
+import Button from "react-bootstrap/Button";
 
 class Experiments extends Component {
     static propTypes = {
@@ -17,9 +18,50 @@ class Experiments extends Component {
         this.props.getExperiments();
     }
 
+    state = {
+        isShowingModal: false,
+        name: ''
+    };
+
+    onChange = e => this.setState({[e.target.name]: e.target.value});
+    handleShow = () => this.setState({isShowingModal: true});
+    handleClose = () => this.setState({isShowingModal: false});
+    handleSubmit = () => {
+        this.setState({
+            isShowingModal: false,
+            name: ''
+        });
+        console.log("new name: " + this.state.name)
+    };
+
+
     render() {
+        const name = this.state.name;
+        //todo fix input field
         return (
             <div>
+                <Modal show={this.state.isShowingModal} onHide={this.handleClose} animation={false}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>New name form</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>New experiment name: </Modal.Body>
+                    <input type="text"
+                           className="form-control"
+                           name="name"
+                           value={name}
+                           placeholder="Enter new name"
+                           onChange={this.onChange}/>
+                    <Modal.Footer>
+                        <button type="button"
+                                className="btn btn-secondary" onClick={this.handleClose}>
+                            Close
+                        </button>
+                        <button type="button"
+                                className="btn btn-primary" onClick={this.handleSubmit}>
+                            Save Changes
+                        </button>
+                    </Modal.Footer>
+                </Modal>
                 <h1>Experiments:</h1>
                 <table className="table table-striped" datapagesize={5}>
                     <thead>
@@ -40,6 +82,12 @@ class Experiments extends Component {
                             <td>{experiment.status}</td>
                             <td>{experiment.date}</td>
                             <td>
+                                <button type="button"
+                                        className="btn btn-primary" onClick={this.handleShow}>
+                                    Change name
+                                </button>
+                            </td>
+                            <td>
                                 <Link to={"/showExperiment"} className={"btn btn-primary"}
                                       onClick={this.props.getExperimentById.bind(this, experiment.id)}>Show</Link>
                             </td>
@@ -53,7 +101,9 @@ class Experiments extends Component {
                     ))}
                     </tbody>
                 </table>
+
             </div>
+
         );
     }
 }
