4cc2050667ad95d90252dbe76b38a597bc5d9d4b
Mateusz Pernal <exomat@wp.pl>
2019-10-08 22:51:22 +0200

    Prod settings

--- decisionTree/settings.py
+++ decisionTree/settings.py
@@ -29,11 +29,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = '1$4)wd60abv_a(l@b=m!hrbwe2@#i(fus=+wej-@68op2r_)rg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "localhost"
+    "localhost",
+    "vps733855.ovh.net",
+    "145.239.91.137"
 ]
 
 # Application definition
@@ -93,23 +95,23 @@ WSGI_APPLICATION = 'decisionTree.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'decisionTree',
-#         'USER': 'decisionTree',
-#         'PASSWORD': 'decisionTree',
-#         'HOST': 'postgres',
-#         'PORT': '5552',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'decisionTree',
+        'USER': 'decisionTree',
+        'PASSWORD': 'decisionTree',
+        'HOST': 'postgres',
+        'PORT': '5552',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -145,22 +147,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'file': {
-#             'level': 'DEBUG',
-#             'class': 'logging.FileHandler',
-#             'filename': BASE_DIR + '/debug.log'
-#         },
-#     },
-#     'loggers': {
-#         '': {
-#             'handlers': ['file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#     },
-# }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR + '/debug.log'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
