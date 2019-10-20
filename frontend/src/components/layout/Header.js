f0413f30f7e42e0926347dcc54b584e4d3397650
Mateusz Pernal <exomat@wp.pl>
2019-10-21 00:19:25 +0200

    fix

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -54,12 +54,9 @@ export class Header extends Component {
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
                 <div className="container">
-                    <a className="nav-item active" href="/">Experiments</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
-                            aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
+                    <li className="nav-item active">
+                        <Link to={"/"} className={"nav-link"}>Experiments</Link>
+                    </li>
                     <div className="collapse navbar-collapse  " id="navbarColor01">
                         {isAuthenticated ? authLinks : guestLinks}
                     </div>
